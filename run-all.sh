242572f398d66ecf710446f1b82cbcef95d5a91b
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-01-25 00:14:21 +0100

    bring up to date

--- run-all.sh
+++ run-all.sh
@@ -1,6 +1,6 @@
+mkdir -p playbooks/files/
 bash run.sh playbooks/create.yml
 sleep 10
 bash run.sh playbooks/install/install-docker.yml
-bash run.sh playbooks/install/install-certbot-pip.yml
 bash run.sh playbooks/install/install-pektin.yml
 bash run.sh playbooks/run/run-pektin.yml
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mkdir -p playbooks/files/priv/
 ansible-playbook -i playbooks/hcloud.yml $1
