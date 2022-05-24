cc2e374cc4aed2ff15c4e71e4336ac25335d22ee
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-05-24 17:43:20 +0200

    add new config fix alpine version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine
+FROM alpine:3.16.0
 RUN apk add --update --no-cache py3-pip ansible bash openssh-keygen openssh-client rsync
 RUN pip3 install hcloud
 WORKDIR /ansible
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
+YOUR_SSH_PUB_KEY=/home/paul/.ssh/blackbox.pub
 # bash create-keys.sh
-docker build . -t pektin-compose-ansible && docker run -it --rm --mount "type=bind,source=$PWD,dst=/ansible/" --mount "type=bind,source=/home/paul/.ssh/blackbox.pub,dst=/root/personal.pub" pektin-compose-ansible bash run-all.sh
+docker build . -t pektin-compose-ansible && docker run -it --rm --mount "type=bind,source=$PWD,dst=/ansible/" --mount "type=bind,source=$YOUR_SSH_PUB_KEY,dst=/root/personal.pub" pektin-compose-ansible bash run-all.sh
